1b1957dfbf058182724a929ce8cf46fa9ad3e635
kumarshyam7777 <kumar.shyam7777@gmail.com>
2022-08-22 16:03:20 +0530

    End of section 1

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,10 @@
+const App = (props) => {
+  return (
+    <div className="App">
+      <h1>Re-vents</h1>
+      <h3>The styles are Global in index.js file</h3>
+    </div>
+  );
+};
+
+export default App;
