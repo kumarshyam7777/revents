f0f712f1d7e5896273cc052b2da319ed76bc3a87
kumarshyam7777 <kumar.shyam7777@gmail.com>
2022-08-30 13:35:17 +0530

    section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React ,{useState}from "react";
+import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
 //import { Button } from "semantic-ui-react";
+import NavBar from "./../../features/nav/NavBar";
 export default function App() {
+  const [formOpen, setFormOpen] = useState(false);
   return (
-    <div>
-      <h1>Re-vents</h1>
-      <EventDashboard/>
-    </div>
+    <>
+      <NavBar setFormOpen={setFormOpen} />
+      <Container className='main'>
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
+    </>
   );
 }
--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,22 +1,41 @@
-import React from 'react'
-import {Menu,Container} from 'semantic-ui-react';
-import { Button } from 'semantic-ui-react';
+import React from "react";
+import { Menu, Container } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
 
-const NavBar = () => {
+const NavBar = ({ setFormOpen }) => {
   return (
-   <Menu inverted fixed='top'>
-    <Container>
+    <Menu inverted fixed='top'>
+      <Container>
         <Menu.Item header>
-            <img src="/assets/logo.png" alt="logo" srcset="" />
-            Re-vents
+          <img
+            src='/assets/logo.png'
+            alt='logo'
+            srcSet=''
+            style={{ marginRight: 15 }}
+          />
+          Re-vents
         </Menu.Item>
-        <Menu name="Evengts"/>
+        <Menu.Item name='Events' />
         <Menu.Item>
-  <Button positive inverted/>
+          <Button
+            onClick={() => setFormOpen(true)}
+            positive
+            inverted
+            content='Create Event'
+          />
         </Menu.Item>
-    </Container>
-   </Menu>
-  )
-}
+        <Menu.Item position='right'>
+          <Button basic inverted content='Login' />
+          <Button
+            basic
+            inverted
+            content='Register'
+            style={{ maginLeft: "0.5em" }}
+          />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
 
-export default NavBar
+export default NavBar;
